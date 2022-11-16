6ab80a332749b0ca26325049b6ccb39a6e8601cb
ibnumalik99 <ibnumalik69384@gmail.com>
2022-11-16 20:52:43 +0700

    portfolio saya

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+$('.scroll').on('click', function(e) {
+
+    var tujuan = $(this).attr('href');
+   
+    var elemenTujuan = $(tujuan);
+   
+    $('html , body').animate({
+     scrollTop: elemenTujuan.offset().top - 50
+    },900,'easeInOutExpo');
+   
+    e.preventDefault();
+   });
